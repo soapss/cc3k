722e2cf68c5225091a047cc59c6568185f603721
soapss <76599851+soapss@users.noreply.github.com>
2022-10-23 20:12:45 -0500

    Delete cc3k.d

--- cc3k.d
+++
@@ -1,7 +0,0 @@
-cc3k.o: cc3k.cc cc3k.h floor.h cell.h entity.h color.h player.h \
- character.h potion.h item.h enemy.h stairway.h gold.h observer.h \
- subject.h message.h level.h defaultlevel.h drow.h shade.h goblin.h \
- troll.h vampire.h orc.h human.h dragon.h dragonHoard.h dwarf.h \
- halfling.h elf.h merchant.h pathfinder.h restoreHealth.h poisonHealth.h \
- boostAtk.h woundAtk.h boostDef.h woundDef.h smallPile.h normalPile.h \
- merchantHoard.h
