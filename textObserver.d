cb3b91034b4ed83efb55499db6550633c1d9c12c
soapss <76599851+soapss@users.noreply.github.com>
2022-11-18 00:45:49 -0600

    Delete textObserver.d

--- textObserver.d
+++
@@ -1,3 +0,0 @@
-textObserver.o: textObserver.cc textObserver.h observer.h cc3k.h floor.h \
- cell.h entity.h color.h player.h character.h potion.h item.h enemy.h \
- stairway.h gold.h subject.h message.h
