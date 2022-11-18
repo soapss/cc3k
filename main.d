12a278f21fad2462842f1d9751a61a6c2ae5a43f
soapss <76599851+soapss@users.noreply.github.com>
2022-11-18 00:41:40 -0600

    Delete main.d

--- main.d
+++
@@ -1,3 +0,0 @@
-main.o: main.cc color.h player.h character.h entity.h potion.h item.h \
- textObserver.h observer.h cc3k.h floor.h cell.h enemy.h stairway.h \
- gold.h subject.h message.h graphicalObserver.h window.h
